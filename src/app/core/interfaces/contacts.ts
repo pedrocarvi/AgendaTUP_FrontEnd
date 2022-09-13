e4bfae1b440e0871a2f2e7e373d4783a1f43a932
Pedro Carvi <pedro.carvi-11@outlook.es>
2022-09-13 19:22:39 -0300

    contacts.ts

---
+++ src/app/core/interfaces/contacts.ts
@@ -0,0 +1,11 @@
+export interface Contact {
+    firstName: string,
+    lastName: string,
+    phoneNumber: string,
+}
+
+export const defaultContact:Contact = {
+    firstName: "First name",
+    lastName: "Last name",
+    phoneNumber: "+5412345678"
+}
