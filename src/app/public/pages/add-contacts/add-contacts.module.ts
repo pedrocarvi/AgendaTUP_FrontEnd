4bc264db9a4241952e456fc601c700a4f09052d8
brunoDiter <bruno.diterlizzi@gmail.com>
2022-10-01 16:39:37 -0300

    Agrego carpeta addContacts

---
+++ src/app/public/pages/add-contacts/add-contacts.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AddContactsRoutingModule } from './add-contacts-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AddContactsRoutingModule
+  ]
+})
+export class AddContactsModule { }
