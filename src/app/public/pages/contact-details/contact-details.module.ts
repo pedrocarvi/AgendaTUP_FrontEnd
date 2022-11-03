bfeca784b34ad8ae2fcf47e7eb9a8709455f267f
Pedro Carvi <pedro.carvi-11@outlook.es>
2022-11-03 19:09:28 +0100

    Agregue detalles del contacto

--- src/app/public/pages/contact-details/contact-details.module.ts
+++ src/app/public/pages/contact-details/contact-details.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ContactDetailsRoutingModule } from './contact-details-routing.module';
 import { ContactDetailsComponent } from './contact-details.component';
 
+import { ContactDetailComponent } from '../../components/contact-detail/contact-detail.component';
 
 @NgModule({
   declarations: [
     ContactDetailsComponent,
+    ContactDetailComponent,
   ],
   imports: [
     CommonModule,
