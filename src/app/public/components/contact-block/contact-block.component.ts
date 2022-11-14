947ecf6f95b9f5ae8960c41800264b5f130d9fe8
brunoDiter <bruno.diterlizzi@gmail.com>
2022-11-14 20:06:43 -0300

    creacion pagina contactBlock

---
+++ src/app/public/components/contact-block/contact-block.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+//componentes
+
+
+@Component({
+  selector: 'app-contact-block',
+  templateUrl: './contact-block.component.html',
+  styleUrls: ['./contact-block.component.scss']
+})
+export class ContactBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
